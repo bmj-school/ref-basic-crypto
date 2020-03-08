a31063aee3a5c3c97ee156a34348fd0e2d953387
MarcusJones <jones.0bj3@gmail.com>
2020-03-08 15:24:04 +0100

    update format string

--- basics.py
+++ basics.py
@@ -1,15 +1,12 @@
-print("Hi")
-
 class bit32:
     def __init__(self, hexval, bits=32):
         assert bits%4 == 0
-
         self.val = hexval
         self.bitwidth = bits
-        self.hexwidth = bits/4
+        self.hexwidth = int(bits/4)
 
     def __repr__(self):
-        return hex(self.val)
+        return "|{}|0x{}|{}|".format(self.int, self.hexstring, self.bitstring)
 
     @property
     def int(self):
@@ -21,8 +18,8 @@ class bit32:
 
     @property
     def hexstring(self):
-        # return '{val:0{width}x}'.format(width=self.hexwidth, val=self.val)
-        return f"{self.val:016x}"
+        return '{val:0{width}x}'.format(width=self.hexwidth, val=self.val)
+        # return f"{self.val:016x}"
 
     @property
     def bits(self):
@@ -31,5 +28,5 @@ class bit32:
 a = bit32(22, 32)
 # a.hex = "asdf"
 print('Value:', a)
-print(a.bitstring)
+# print(a.bitstring)
 # print(a.hexstring)
