84950d5f52598d860ecb1ea5dc2148ca2c811215
MarcusJones <jones.0bj3@gmail.com>
2020-03-08 15:17:47 +0100

    add basic bitstring class

---
+++ basics.py
@@ -0,0 +1,35 @@
+print("Hi")
+
+class bit32:
+    def __init__(self, hexval, bits=32):
+        assert bits%4 == 0
+
+        self.val = hexval
+        self.bitwidth = bits
+        self.hexwidth = bits/4
+
+    def __repr__(self):
+        return hex(self.val)
+
+    @property
+    def int(self):
+        return(self.val)
+
+    @property
+    def bitstring(self):
+        return '{val:0{width}b}'.format(width=self.bitwidth, val=self.val)
+
+    @property
+    def hexstring(self):
+        # return '{val:0{width}x}'.format(width=self.hexwidth, val=self.val)
+        return f"{self.val:016x}"
+
+    @property
+    def bits(self):
+        return bin(self.val)
+
+a = bit32(22, 32)
+# a.hex = "asdf"
+print('Value:', a)
+print(a.bitstring)
+# print(a.hexstring)
